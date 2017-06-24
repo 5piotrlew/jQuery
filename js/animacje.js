d0523c45dea9111adae51a1eb1c54d1f01b6480a
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-24 16:45:18 +0200

    iiii

--- js/animacje.js
+++ js/animacje.js
@@ -10,7 +10,13 @@ $(function () {
 
     $('h2').slideUp(3000).slideDown(3000);
 
-$('h3').animate({'font-size': '3em', 'margin-left': '100px'}, 2000);
+$('h3').animate({'font-size': '3em', 'margin-left': '100px'}, 2000, animacja);
+    
+    function animacja(){
+        console.log("zakonczono pierwsza animacje. Zaraz startuje druga :)");
+        
+        $('h3').animate({'font-size': '1em', 'margin-left': '0px'}, 2000);
+    }
 
 
 
