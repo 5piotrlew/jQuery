8e3cbec3536b572bc9e3ac91015adc3bb16b2370
Piotr Lewinski <piotrlew5@wp.pl>
2017-06-24 16:03:18 +0200

    animate

---
+++ js/animacje.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+$(function () {
+
+    //$('p').hide(3000).show(3000);
+
+
+    $('h1').fadeOut(3000).fadeIn(3000);
+
+    $('h2').slideUp(3000).slideDown(3000);
+
+$('h3').animate({'font-size': '3em', 'margin-left': '100px'}, 2000);
+
+
+
+
+});
